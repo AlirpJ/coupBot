7e030fa4374020de564573dc0582ea27c8febab1
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-08-09 15:33:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+// Joshua Prila, coupBot
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const prefix = '-';
 const fs = require('fs');
+const { start } = require('repl');
 
 const PLAYER =  '735176300335202345';
 const DEAD =    '735176229899993202';
-
-
+let parts = new Array ();
+var coupBegin = false;
+var playerCount = 0;
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -24,7 +27,7 @@ client.once('ready', () => {
 // Command Line: "node ." to start bot
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-    const parts = message.content.split(' ');
+    parts = message.content.split(' ');
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -33,6 +36,8 @@ client.on('message', message =>{
     if (parts[0] == 'role'){
         if(parts[1] == 'ready'){
             message.member.addRole(PLAYER);
+            message.reply(' you are readied up!');
+            playerCount++;
         }
     }
 
@@ -46,13 +51,23 @@ client.on('message', message =>{
     else if(command == 'test'){
         console.log(message.guild.roles);
         console.log("");
+        message.channel.send("Information has been sent to console!");
     }
+
+
+
 
 });
 
 
+// Start after hitting a certain number of players
+//may change later to have someone input command to start (original game uses anywhere from 2-6 players)
+if(playerCount === 4 && coupBegin == true){
+    client.commands.get('coupStart').execute(message, args);
+}
 
 
 
 // This login must be the last line 
-client.login(token);
+// Token for discord bot. DO NOT SHARE
+client.login('token');

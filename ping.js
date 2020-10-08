fcba36522d4b8a47ce657f0d467c5e545d9d1cd4
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-10-07 19:41:19 -0500

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,9 +1,9 @@
+// Joshua Prila, coupBot
 module.exports = {
     name: 'ping',
-    description: 'This is the ping command',
+    description: 'This is the ping command. Used for testing purposes.',
     execute(message, args){
 
         message.channel.send('pong!');
-
     }
 }
